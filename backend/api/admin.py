662702f37d65c5872b683f366b4da4b85e9bbd65
Tyler <tyler.thompson@tufts.edu>
2021-10-07 16:14:52 -0400

    Add REST requirements. Add REST config in django settings. Add example student model and serializer. Add api view to get db data from api. Add student model to django admin.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from api.models import Student
 
-# Register your models here.
+admin.site.register(Student)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from api.models import Student
+from api.serializers import StudentSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
