318d3c0dedd089499ecd65d6c0f1fdba66ccff56
Tyler <tyler.thompson@tufts.edu>
2021-10-14 19:23:48 -0400

    frontend spice

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -6,11 +6,24 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 export default function Home({ data }) {
+  function getYear(yearNum) {
+    return ['freshman', 'sophomore', 'junior', 'senior'][yearNum];
+  }
+
   return (
     <div className={cx('base')}>
       <h1>Hello, TUPIT!</h1>
       <h1>
-        My name is {data.data[0].attributes.name} and I am {data.data[0].attributes.age} years old
+        {data.data.map((data, i) => (
+          <div key={i}>
+            My name is {data.attributes.first_name} {data.attributes.last_name} and I am a{' '}
+            {data.attributes.age} year old {getYear(data.attributes.year)}. Here are some fun facts:
+            <br />
+            {data.attributes.fun_facts.map((fun_fact, j) => (
+              <div key={j}> - {fun_fact}</div>
+            ))}
+          </div>
+        ))}
       </h1>
     </div>
   );
@@ -19,7 +32,7 @@ export default function Home({ data }) {
 export async function getServerSideProps(context) {
   context.params; // silence unused var warning
 
-  const res = await fetch(`http://localhost:8000/api/students/`);
+  const res = await fetch(`http://localhost:8000/api/team_leaders/`);
   const data = await res.json();
 
   return {
