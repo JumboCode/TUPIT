c77058369a40ad34b16100785ec1dadb2628b6a6
Tyler <tyler.thompson@tufts.edu>
2021-10-21 15:46:59 -0400

    Consolidate first and last name patches

--- frontend/src/pages/nameform.jsx
+++ frontend/src/pages/nameform.jsx
@@ -1,10 +1,14 @@
 import Router from 'next/router';
 
 export default function NameForm({ leader }) {
-  const submitFirst = async (event) => {
+  const submit = async (event) => {
     event.preventDefault();
 
-    console.log('Received: ' + event.target.name.value);
+    let first = event.target.first.value;
+    if (first == '') first = leader.attributes.first_name;
+
+    let last = event.target.last.value;
+    if (last == '') last = leader.attributes.last_name;
 
     const res = await fetch('http://localhost:8000/api/team_leaders/' + leader.id + '/', {
       method: 'PATCH',
@@ -16,32 +20,8 @@ export default function NameForm({ leader }) {
           type: 'TeamLeader',
           id: leader.id,
           attributes: {
-            first_name: event.target.first.value,
-          },
-        },
-      }),
-    });
-
-    console.log(res);
-    Router.push('');
-  };
-
-  const submitLast = async (event) => {
-    event.preventDefault();
-
-    console.log('Received: ' + event.target.name.value);
-
-    const res = await fetch('http://localhost:8000/api/team_leaders/' + leader.id + '/', {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/vnd.api+json',
-      },
-      body: JSON.stringify({
-        data: {
-          type: 'TeamLeader',
-          id: leader.id,
-          attributes: {
-            last_name: event.target.last.value,
+            first_name: first,
+            last_name: last,
           },
         },
       }),
@@ -53,23 +33,10 @@ export default function NameForm({ leader }) {
 
   return (
     <div>
-      <form onSubmit={submitFirst}>
-        <input
-          id="first"
-          type="text"
-          placeholder={leader.attributes.first_name}
-          defaultValue=""
-          required
-        />
-      </form>
-      <form onSubmit={submitLast}>
-        <input
-          id="last"
-          type="text"
-          placeholder={leader.attributes.last_name}
-          defaultValue=""
-          required
-        />
+      <form onSubmit={submit}>
+        <input id="first" type="text" placeholder={leader.attributes.first_name} defaultValue="" />
+        <input id="last" type="text" placeholder={leader.attributes.last_name} defaultValue="" />
+        <button type="submit">SUBMIT</button>
       </form>
     </div>
   );
