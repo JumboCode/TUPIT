f7656e2dbd3dd143c37334ac8e34680f044e27a7
Sara Francis <sarafrancis@MacBook-Air-49.local>
2021-10-25 11:20:51 -0400

    iadding dropdown stuff#

--- frontend/src/pages/class/index.jsx
+++ frontend/src/pages/class/index.jsx
@@ -1,43 +1,59 @@
 import React, { Component } from 'react';
-
-// TODO: Implement Search and Requirement component. 
+import styles from './class.module.scss';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
+// TODO: Implement Search and Requirement component.
 // I think we will implement a box and click component as util to pass in text.
 
 class Search extends Component {
-    constructor(props) {
-        super(props);
-    }
-} 
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section>
+        <h1>Search</h1>
+        <div className={styles.dropdown}>
+          <button className={styles.dropbtn}> Dropdown </button>
+          <div className={styles.dropdown_content}>
+            <a href="#">Link 1</a>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 
 class Requirement extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            //TODO: Implement fetch to grab the requirements for a course
-            //and display it
-            requirement: ["COMP11", "COMP15", "COMP160"]
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      //TODO: Implement fetch to grab the requirements for a course
+      //and display it
+      requirement: ['COMP11', 'COMP15', 'COMP160'],
+    };
+  }
 
-    render() {
-        return (
-            <section>
-                <h1>Requirements</h1>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section>
+        <h1>Requirements</h1>
+      </section>
+    );
+  }
 }
 
 export default class Class extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <main>
-                < Requirement />
-            </main>
-        );
-    }
+  render() {
+    return (
+      <main>
+        <Search />
+      </main>
+    );
+  }
 }
