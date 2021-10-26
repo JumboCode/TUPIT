283eaf04e29e7385ac5aa9aec631a695e8382ebe
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-10-26 12:44:08 -0400

    Tentatively add fields to Student model
    For a couple of fields that need more info to implement, max_length=32
    was used. Also, placeholders for the unimplemented Class and Grade
    models were used. This has not been tested.

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,9 +1,33 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+import re
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator, MinValueValidator
+from django.utils.translation import gettext_lazy as _
+
 
 class Student(models.Model):
-    name = models.CharField(max_length=32)
-    age = models.IntegerField()
-    courses = ArrayField(
-        models.CharField(max_length=16)
-    )
+    def validate_doc_num(value):
+        regex = re.compile('^W\d+$')
+        if not regex.match(value):
+            raise ValidationError(
+                _('%(value)s does not match the format for a doc_num'),
+                params={'value': value},
+            )
+
+    firstname = models.CharField(max_length=32)
+    lastname = models.CharField(max_length=32)
+    birthday = models.DateField(null=True, blank=True)
+    doc_num = models.CharField(max_length=32, validators=[validate_doc_num])
+    tufts_num = models.CharField(max_length=7, validators=[
+                                 MinLengthValidator(7)])
+    bhcc_num = models.CharField(max_length=32)
+    parole_status = models.TextField(max_length=256)
+    student_status = models.TextField(max_length=256)
+
+    validate_nonnegative = MinValueValidator(0)
+    cohort = models.IntegerField(validators=[validate_nonnegative])
+    years_given = models.IntegerField(validators=[validate_nonnegative])
+    years_left = models.IntegerField(validators=[validate_nonnegative])
+    # classes = models.ManyToManyField()
+    # grades = modesl.ManyToManyField()
