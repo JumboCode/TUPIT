9fd91662f9caf978b8494018caff04e0ffcb0f92
Jason Xu <gwangsenshoe@gmail.com>
2021-10-28 20:49:19 -0400

    Adding a course model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,9 +1,59 @@
-from django.db import models
+from django.db.models import Model, CharField, IntegerField, ManyToManyField
 from django.contrib.postgres.fields import ArrayField
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
+from django.utils.translation import gettext_lazy as _
 
 class Student(models.Model):
-    name = models.CharField(max_length=32)
-    age = models.IntegerField()
+    name = CharField(max_length=32)
+    age = IntegerField()
     courses = ArrayField(
-        models.CharField(max_length=16)
+        CharField(max_length=16)
+    )
+
+class Course(Model):
+    # validators and cleaning
+    validate_nonnegative = MinValueValidator(0)
+    def validate_nonzero_length(value):
+        if len(value) == 0:
+            raise ValidationError('Must have at least one instructor')
+
+    def clean(self):
+        super(Course, self).clean()
+        if self.course_number_tufts == "" and self.course_number_bhcc == "":
+            raise ValidationError('Both course numbers cannot be None')
+
+    course_title = CharField(max_length=32, blank=False)
+
+    course_number_tufts = CharField(max_length=32)
+    course_number_bhcc = CharField(max_length=32)
+
+    credits_tufts = IntegerField(validators=[validate_nonnegative])
+    credits_bhcc = IntegerField(validators=[validate_nonnegative])
+
+    # department field of many
+    COMPUTER_SCIENCE = 'COMP'
+    COGNITIVE_STUDIES = 'COGS'
+    MATH = 'MATH'
+    PSYCHOLOGY = 'PSYC'
+    DEPARTMENTS = [
+        (COMPUTER_SCIENCE, 'Computer Science'),
+        (COGNITIVE_STUDIES, 'Cognitive Studies'),
+        (MATH, 'Math'),
+        (PSYCHOLOGY, 'Psychology'),
+    ]
+    department = CharField(
+        max_length=4,
+        choices=DEPARTMENTS,
+        default=None
+    )
+
+    prereqs = ManyToManyField(
+        "self",
+        related_name='+',
+    )
+
+    instructors = ArrayField(
+        CharField(max_length=32, blank=False),
+        validators = [validate_nonzero_length]
     )
