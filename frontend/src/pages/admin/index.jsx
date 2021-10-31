41f33e9d53a4f5f80d0d70f7f8d8aa1748dbcf14
Michael Simpson <michael.simpson@tufts.edu>
2021-10-31 19:14:45 -0400

    styling and organization updates

--- frontend/src/pages/admin/index.jsx
+++ frontend/src/pages/admin/index.jsx
@@ -10,27 +10,66 @@ const Admin = () => {
     <>
       <div className={cx('admin-content')}>
         <div className={cx('admin-control')}>
-          <div className={cx('admin-control-profile')}>
-            <p>Profile</p>
-          </div>
-          <div className={cx('admin-control-buttons')}>
-            <div className={cx('admin-control-buttons-item')}>
-              <p>students</p>
-            </div>
-            <div className={cx('admin-control-buttons-item')}>
-              <p>class</p>
-            </div>
-          </div>
+          <ControlContainer />
         </div>
         <div className={cx('admin-display')}>
-          <p>display</p>
-          <div className={cx('admin-display-list')}></div>
-          <div className={cx('admin-display-add-button')}>
-            <p>add</p>
-          </div>
+          <DisplayContainer />
         </div>
       </div>
     </>
+  );
+};
+
+const ControlContainer = () => {
+  return (
+    <div className={cx('admin-control-container')}>
+      <div className={cx('admin-control-profile')}>
+        <p>
+          Profile
+          <br />
+          <br />
+          <br />
+          <br />
+          hi
+        </p>
+      </div>
+      <div className={cx('admin-control-buttons')}>
+        <div className={cx('admin-control-buttons-item')}>
+          <p>students</p>
+        </div>
+        <div className={cx('admin-control-buttons-item')}>
+          <p>class</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const DisplayContainer = () => {
+  const DisplayListItem = (props) => {
+    return (
+      <div className={cx('admin-display-list-item')}>
+        <li>
+          Item
+          <br />
+          {props.num}
+        </li>
+      </div>
+    );
+  };
+  return (
+    <div className={cx('admin-display-container')}>
+      <div className={cx('admin-display-list')}>
+        <ul>
+          {Array.from({ length: 20 }, (_, i) => i).map((num) => {
+            return <DisplayListItem num={num} key={num} />;
+          })}
+        </ul>
+        <div className={cx('admin-display-add-button')}>
+          <p>add</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
