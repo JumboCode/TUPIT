0e826dc5aa54fa29c1f2de325d77f41434474224
Jason Xu <gwangsenshoe@gmail.com>
2021-11-01 14:49:42 -0400

    adding serializer, view, and url

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework_json_api import serializers
-from api.models import Student
+from api.models import Student, Course
 
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Student
         fields = ('name', 'age', 'courses')
+
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('course_title', 'course_number_tufts', 'course_number_bhcc', 'credits_tufts', 'credits_bhcc', 'department', 'prereqs', 'instructors')
