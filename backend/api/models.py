c3542f55470290c2878848c259027bef89d029e3
Jason Xu <39848525+jason-shoe@users.noreply.github.com>
2021-11-01 16:19:50 -0400

    Adding a course model (#2)
    * Adding a course model
    
    * Adding course serializer, view, and url

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,9 +1,61 @@
-from django.db import models
+from django.db.models import Model, CharField, IntegerField, ManyToManyField
 from django.contrib.postgres.fields import ArrayField
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
+from django.utils.translation import gettext_lazy as _
 
-class Student(models.Model):
-    name = models.CharField(max_length=32)
-    age = models.IntegerField()
+class Student(Model):
+    name = CharField(max_length=32)
+    age = IntegerField()
     courses = ArrayField(
-        models.CharField(max_length=16)
+        CharField(max_length=16)
+    )
+
+class Course(Model):
+    # validators and cleaning
+    validate_nonnegative = MinValueValidator(0)
+
+    def clean(self):
+        super(Course, self).clean()
+        if self.course_number_tufts == "" and self.course_number_bhcc == "":
+            raise ValidationError('Both course numbers cannot be None')
+
+    course_title = CharField(max_length=32, blank=False)
+
+    course_number_tufts = CharField(max_length=32, blank=True, null=True)
+    course_number_bhcc = CharField(max_length=32, blank=True, null=True)
+
+    credits_tufts = IntegerField(validators=[validate_nonnegative], blank=True, null=True)
+    credits_bhcc = IntegerField(validators=[validate_nonnegative], blank=True, null=True)
+
+    # department field of many
+    COMPUTER_SCIENCE = 'COMP'
+    COGNITIVE_STUDIES = 'COGS'
+    MATH = 'MATH'
+    PSYCHOLOGY = 'PSYC'
+    DEPARTMENTS = [
+        (COMPUTER_SCIENCE, 'Computer Science'),
+        (COGNITIVE_STUDIES, 'Cognitive Studies'),
+        (MATH, 'Math'),
+        (PSYCHOLOGY, 'Psychology'),
+    ]
+    department = CharField(
+        max_length=4,
+        choices=DEPARTMENTS,
+        blank=True,
+        null=True,
+    )
+
+    prereqs = ManyToManyField(
+        "self",
+        related_name='+',
+        symmetrical=False,
+        blank=True,
+        default=None,
+    )
+
+    instructors = ArrayField(
+        CharField(max_length=32, blank=False),
+        blank=True,
+        null=True,
     )
