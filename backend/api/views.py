e917a5593db087ae84f6824c9ebb4d51d0154dd2
Jason Xu <39848525+jason-shoe@users.noreply.github.com>
2021-11-01 16:19:50 -0400

    Adding a course model (#2)
    * Adding a course model
    
    * Adding course serializer, view, and url

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,11 @@
-from api.models import Student
-from api.serializers import StudentSerializer
+from api.models import Student, Course
+from api.serializers import StudentSerializer, CourseSerializer
 from rest_framework import viewsets
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
