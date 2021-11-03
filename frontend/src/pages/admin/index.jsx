808587dbda5aba3622c83a0f1940035d10f83904
Michael Simpson <michael.simpson@tufts.edu>
2021-11-03 17:23:26 -0400

    Working commit for final styling!
    Co-authored-by: sarahsvahn <sarahsvahn@users.noreply.github.com>
    Co-authored-by: sgilfeather <sgilfeather@users.noreply.github.com>

--- frontend/src/pages/admin/index.jsx
+++ frontend/src/pages/admin/index.jsx
@@ -5,22 +5,46 @@ import styles from './index.module.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
+const getListData = (isClass, studentNum, classNum) => {
+  const length = isClass ? classNum : studentNum;
+  return Array.from({ length: length }, (_, i) => i).map((num) => {
+    return {
+      id: num,
+      name: `${isClass ? 'Class' : 'Student'} ${num}`,
+    };
+  });
+};
+
 const Admin = () => {
+  const [isClass, setIsClass] = React.useState(true);
+  const [studentNum, setStudentNum] = React.useState(20);
+  const [classNum, setClassNum] = React.useState(10);
   return (
     <>
+      <style jsx global>{`
+        body {
+          background-color: lightblue;
+        }
+      `}</style>
       <div className={cx('admin-content')}>
         <div className={cx('admin-control')}>
-          <ControlContainer />
+          <ControlContainer setIsClass={setIsClass} />
         </div>
         <div className={cx('admin-display')}>
-          <DisplayContainer />
+          <DisplayContainer
+            isClass={isClass}
+            studentNum={studentNum}
+            setStudentNum={setStudentNum}
+            classNum={classNum}
+            setClassNum={setClassNum}
+          />
         </div>
       </div>
     </>
   );
 };
 
-const ControlContainer = () => {
+const ControlContainer = ({ setIsClass }) => {
   return (
     <div className={cx('admin-control-container')}>
       <div className={cx('admin-control-profile')}>
@@ -34,10 +58,10 @@ const ControlContainer = () => {
         </p>
       </div>
       <div className={cx('admin-control-buttons')}>
-        <div className={cx('admin-control-buttons-item')}>
+        <div className={cx('admin-control-buttons-item')} onClick={() => setIsClass(false)}>
           <p>students</p>
         </div>
-        <div className={cx('admin-control-buttons-item')}>
+        <div className={cx('admin-control-buttons-item')} onClick={() => setIsClass(true)}>
           <p>class</p>
         </div>
       </div>
@@ -45,15 +69,27 @@ const ControlContainer = () => {
   );
 };
 
-const DisplayContainer = () => {
+const DisplayContainer = ({ isClass, studentNum, setStudentNum, classNum, setClassNum }) => {
   const DisplayListItem = (props) => {
     return (
       <div className={cx('admin-display-list-item')}>
         <li>
           Item
           <br />
-          {props.num}
+          {props.name}
         </li>
+      </div>
+    );
+  };
+  const AddButton = ({ isClass, addToStudentNum, addToClassNum }) => {
+    return (
+      <div
+        className={cx('admin-display-add-button')}
+        onClick={isClass ? addToClassNum : addToStudentNum}
+      >
+        <button>
+          <p>Add {isClass ? 'class' : 'student'}</p>
+        </button>
       </div>
     );
   };
@@ -61,13 +97,15 @@ const DisplayContainer = () => {
     <div className={cx('admin-display-container')}>
       <div className={cx('admin-display-list')}>
         <ul>
-          {Array.from({ length: 20 }, (_, i) => i).map((num) => {
-            return <DisplayListItem num={num} key={num} />;
+          {getListData(isClass, studentNum, classNum).map((el) => {
+            return <DisplayListItem key={el.id} name={el.name} />;
           })}
         </ul>
-        <div className={cx('admin-display-add-button')}>
-          <p>add</p>
-        </div>
+        <AddButton
+          isClass={isClass}
+          addToStudentNum={() => setStudentNum(studentNum + 1)}
+          addToClassNum={() => setClassNum(classNum + 1)}
+        />
       </div>
     </div>
   );
