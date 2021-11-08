92fb893c7e7180dbf9f30141fd2750980e13c772
Michael Simpson <michael.simpson@tufts.edu>
2021-11-08 12:28:47 -0500

    convert index demo page to ts

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,10 +5,12 @@ import styles from './index.module.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <div className={cx('base')}>
       <h1>Hello, TUPIT!</h1>
     </div>
   );
-}
+};
+
+export default Home;
