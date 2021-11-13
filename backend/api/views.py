4c7895c1d19caaa57edc80583792a1a8986574d8
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-11-13 13:08:19 -0500

    Serializer and URLs for Degree

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,5 @@
-from api.models import Student, Course, CourseProgress
-from api.serializers import StudentSerializer, CourseSerializer, CourseProgressSerializer
+from api.models import Student, Course, CourseProgress, Degree
+from api.serializers import StudentSerializer, CourseSerializer, CourseProgressSerializer, DegreeSerializer
 from rest_framework import viewsets
 
 class StudentViewSet(viewsets.ModelViewSet):
@@ -13,3 +13,7 @@ class CourseViewSet(viewsets.ModelViewSet):
 class CourseProgressViewSet(viewsets.ModelViewSet):
     queryset = CourseProgress.objects.all()
     serializer_class = CourseProgressSerializer
+
+class DegreeViewSet(viewsets.ModelViewSet):
+    queryset = Degree.objects.all()
+    serializer_class = DegreeSerializer
