1aa2fc35bd6b905f4970e05abc4817ffb8197a1f
Tyler <tyler.thompson@tufts.edu>
2021-11-22 12:38:56 -0500

    User Auth (#10)
    * Initial user auth functionality

--- backend/api/views.py
+++ backend/api/views.py
@@ -17,3 +17,47 @@ class CourseProgressViewSet(viewsets.ModelViewSet):
 class DegreeViewSet(viewsets.ModelViewSet):
     queryset = Degree.objects.all()
     serializer_class = DegreeSerializer
+
+import json
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.middleware.csrf import get_token
+from django.views.decorators.csrf import csrf_exempt
+
+def GetCSRFToken(request):
+    response = JsonResponse({
+        'info': 'Successfully set CSRF token',
+        }, status=200)
+    token = get_token(request)
+    response['X-CSRFToken'] = token
+    return response
+
+def LoginUser(request):
+    data = json.loads(request.body)
+    username = data.get('username')
+    password = data.get('password')
+
+    if username == '' or password == '':
+        return JsonResponse({'info': 'Missing username or password'}, status=400)
+    
+    user = authenticate(request, username=username, password=password)
+
+    if user is None:
+        return JsonResponse({'info': 'Invalid credentials'}, status=401)
+    
+    login(request, user)
+    response = JsonResponse({'info': 'Successfully authenticated'}, status=200)
+    return response
+
+@csrf_exempt
+def LogoutUser(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return JsonResponse({'info': 'Successfully logged out'}, status=200)
+
+@csrf_exempt
+def ValidateLoggedIn(request):
+    if request.user.is_authenticated:
+        return JsonResponse({'is_logged_in': True}, status=200)
+    else:
+        return JsonResponse({'is_logged_in': False}, status=200)
---
+++ frontend/src/components/auth.tsx
@@ -0,0 +1,121 @@
+import { checkPropTypes } from 'prop-types';
+import React, { useState, useEffect, useContext } from 'react';
+
+export const AuthContext = React.createContext({
+  isLoggedIn: false,
+  csrfToken: '',
+  login: async (username, password): Promise<void> => {
+    return new Promise((resolve, reject) => reject());
+  },
+  logout: async (): Promise<void> => {
+    return new Promise((resolve, reject) => reject());
+  },
+});
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const [csrfToken, setCsrfToken] = useState('');
+
+  useEffect(() => {
+    // Get and store CSRF token
+    fetch('http://127.0.0.1:8000/get-csrf-token/', { credentials: 'include' })
+      .then((res) => setCsrfToken(res.headers.get('X-CSRFToken')))
+      .catch((err) => console.log(err));
+
+    // Check if user is logged in and set isLoggedIn state
+    checkAuth().then(
+      (res) => setIsLoggedIn(res),
+      (err) => console.log(err)
+    );
+  }, [isLoggedIn]);
+
+  const login = async (username: string, password: string): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      fetch('http://127.0.0.1:8000/login/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrfToken,
+        },
+        credentials: 'include',
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      })
+        .then((res) => {
+          if (res.status === 200) {
+            setIsLoggedIn(true);
+            resolve();
+          } else {
+            setIsLoggedIn(false);
+            reject('Invalid login credentials');
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          reject('Error connecting to server');
+        });
+    });
+  };
+
+  const checkAuth = async (): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+      fetch('http://127.0.0.1:8000/validate-logged-in/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrfToken,
+        },
+        credentials: 'include',
+      })
+        .then((res) => {
+          res.json().then((data) => {
+            resolve(data.is_logged_in);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          reject('Error connecting to server');
+        });
+    });
+  };
+
+  const logout = async (): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      fetch('http://127.0.0.1:8000/logout/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrfToken,
+        },
+        credentials: 'include',
+      })
+        .then((res) => {
+          setIsLoggedIn(res.status === 200);
+          resolve();
+        })
+        .catch((err) => {
+          console.log(err);
+          reject('Error connecting to server');
+        });
+    });
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        csrfToken,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
