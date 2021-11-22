987365cf5e76c76399f893fd2bbba1378a2151fe
Tyler <tyler.thompson@tufts.edu>
2021-11-22 12:38:56 -0500

    User Auth (#10)
    * Initial user auth functionality

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import AuthProvider from '../components/auth';
 
 import '../styles/global/base.scss';
 
 // _app.jsx should not be converted to typescript
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <AuthProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
       </Head>
 
       <Component {...pageProps} />
-    </>
+    </AuthProvider>
   );
 }
 
