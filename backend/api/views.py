d26ee8df931274fa0b076ab083797c3770cf9705
Nick Doan <nickbar01234@gmail.com>
2021-11-30 11:34:40 -0500

    Add search filter in Django REST API (#12)
    * Add search filter in Django REST API
    
    * Change search filter to regex to allow for spaces in course title, move search filter to top of filter list
    
    * Add __str__ method to models
    
    When an object is printed to the terminal, it first calls the __str__
    method. In this case, we would like to see the specifics attribute of
    the object we retrieve.
    
    * Add .vscode to .gitignore
    
    * Add filter options to ViewSet
    
    * Update filter method
    
    Co-authored-by: Tyler Thompson <ty.9100@gmail.com>

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,45 @@
 from api.models import Student, Course, CourseProgress, Degree
-from api.serializers import StudentSerializer, CourseSerializer, CourseProgressSerializer, DegreeSerializer
-from rest_framework import viewsets
+from api.serializers import *
+from api.filters import *
+
+from django.db.models.base import ModelBase
+from rest_framework import viewsets, filters 
+from django_filters.rest_framework import DjangoFilterBackend
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter) 
+    ordering_fields = ('firstname', 'lastname')
+    filterset_class = StudentFilterSet
+
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+
+    # It is a good practice to include field that can be ordered by to prevent
+    # information leakage. 
+    ordering_fields = ('course_number_tufts', 'course_number_bhcc')
+    filterset_class = CourseFilterSet
+
+    def get_queryset(self):
+        queryset = self.queryset
+        # request is a dictionary with key being the parameter 
+        if len(self.request.query_params) == 0:
+            return queryset.order_by('course_number_tufts')
+        return queryset.order_by('course_number_tufts')
+
 class CourseProgressViewSet(viewsets.ModelViewSet):
     queryset = CourseProgress.objects.all()
     serializer_class = CourseProgressSerializer
+
+    filter_backends = (DjangoFilterBackend, )
+    filterset_class = CourseProgressFilterSet
+
 
 class DegreeViewSet(viewsets.ModelViewSet):
     queryset = Degree.objects.all()
