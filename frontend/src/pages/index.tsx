0fd23ec49e18311ff2fb394f3bdef621ca5f6685
Jason Xu <gwangsenshoe@gmail.com>
2021-12-02 20:55:27 -0500

    Beautiful work from Jason 10/10 would reccomend

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 // Import styles
 import styles from './index.module.scss';
 import classNames from 'classnames/bind';
+import { CourseForm } from './CourseForm';
 const cx = classNames.bind(styles);
 
 const Home: React.FC = () => {
   return (
     <div className={cx('base')}>
+      <CourseForm />
       <h1>Hello, TUPIT!</h1>
     </div>
   );
