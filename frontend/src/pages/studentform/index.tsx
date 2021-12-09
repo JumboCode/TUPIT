f5c8063192b216ad5b3003cffd74f97095ff7734
taarikabala1 <taarikabala@gmail.com>
2021-12-09 11:51:01 -0500

    Early styling: light blue background, centering, font

--- frontend/src/pages/studentform/index.tsx
+++ frontend/src/pages/studentform/index.tsx
@@ -21,147 +21,151 @@ const StudentForm: React.FC = () => {
   });
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        fetch('http://localhost:8000/api/students/', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/vnd.api+json',
-          },
-          body: JSON.stringify({
-            data: {
-              type: 'Student',
-              attributes: { ...student },
+    <>
+      <p className={cx('title')}>Enter a Student's Information!</p>
+      <form
+        className={cx('student-form')}
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetch('http://localhost:8000/api/students/', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/vnd.api+json',
             },
-          }),
-        });
-      }}
-    >
-      <label>
-        First name:{' '}
-        <input
-          type="text"
-          name="firstname"
-          value={student.firstname}
-          onChange={(e) => setStudent({ ...student, firstname: e.target.value })}
-        />
-      </label>
-      <br />
-      <label>
-        Last name:{' '}
-        <input
-          type="text"
-          name="lastname"
-          value={student.lastname}
-          onChange={(e) => setStudent({ ...student, lastname: e.target.value })}
-        />
-      </label>
-      <br />
-      <label>
-        Birthday:{' '}
-        <input
-          type="date"
-          name="birthday"
-          value={student.birthday}
-          onChange={(e) => setStudent({ ...student, birthday: e.target.value })}
-        />
-      </label>
-      <br />
-      <label>
-        Doc number:{' '}
-        <input
-          type="text"
-          name="doc_num"
-          value={student.doc_num}
-          onChange={(e) => {
-            const incoming = e.target.value == '' ? 'W' : e.target.value;
-            setStudent({ ...student, doc_num: incoming });
-          }}
-        />
-      </label>
-      <br />
-      <label>
-        Tufts number:{' '}
-        <input
-          type="text"
-          name="tufts_num"
-          maxLength={7}
-          value={student.tufts_num}
-          onChange={(e) => setStudent({ ...student, tufts_num: e.target.value })}
-        />
-      </label>
-      <br />
-      <label>
-        BHCC number:{' '}
-        <input
-          type="text"
-          name="bhcc_num"
-          value={student.bhcc_num}
-          maxLength={32}
-          onChange={(e) => setStudent({ ...student, bhcc_num: e.target.value })}
-        />
-      </label>
-      <br />
-      <label>
-        Parole status:{' '}
-        <textarea
-          name="parole_status"
-          value={student.parole_status}
-          onChange={(e) => setStudent({ ...student, parole_status: e.target.value })}
-        />
-      </label>
-      <br />
-      <label>
-        Student status:{' '}
-        <textarea
-          name="student_status"
-          value={student.student_status}
-          onChange={(e) => setStudent({ ...student, student_status: e.target.value })}
-        ></textarea>
-      </label>
-      <br />
-      <label>
-        Cohort:{' '}
-        <input
-          type="number"
-          name="cohort"
-          value={student.cohort}
-          onChange={(e) => {
-            const incoming: number = e.target.value == '' ? 0 : parseInt(e.target.value);
-            setStudent({ ...student, cohort: incoming });
-          }}
-        />
-      </label>
-      <br />
-      <label>
-        Year given:{' '}
-        <input
-          type="number"
-          name="years_given"
-          value={student.years_given}
-          onChange={(e) => {
-            const incoming: number = e.target.value == '' ? 0 : parseInt(e.target.value);
-            setStudent({ ...student, years_given: incoming });
-          }}
-        />
-      </label>
-      <br />
-      <label>
-        Years left:{' '}
-        <input
-          type="number"
-          name="years_left"
-          value={student.years_left}
-          onChange={(e) => {
-            const incoming: number = e.target.value == '' ? 0 : parseInt(e.target.value);
-            setStudent({ ...student, years_left: incoming });
-          }}
-        />
-      </label>
-      <br />
-      <input type="submit" value="Submit" />
-    </form>
+            body: JSON.stringify({
+              data: {
+                type: 'Student',
+                attributes: { ...student },
+              },
+            }),
+          });
+        }}
+      >
+        <label>
+          First name:{' '}
+          <input
+            type="text"
+            name="firstname"
+            value={student.firstname}
+            onChange={(e) => setStudent({ ...student, firstname: e.target.value })}
+          />
+        </label>
+        <br />
+        <label>
+          Last name:{' '}
+          <input
+            type="text"
+            name="lastname"
+            value={student.lastname}
+            onChange={(e) => setStudent({ ...student, lastname: e.target.value })}
+          />
+        </label>
+        <br />
+        <label>
+          Birthday:{' '}
+          <input
+            type="date"
+            name="birthday"
+            value={student.birthday}
+            onChange={(e) => setStudent({ ...student, birthday: e.target.value })}
+          />
+        </label>
+        <br />
+        <label>
+          Doc number:{' '}
+          <input
+            type="text"
+            name="doc_num"
+            value={student.doc_num}
+            onChange={(e) => {
+              const incoming = e.target.value == '' ? 'W' : e.target.value;
+              setStudent({ ...student, doc_num: incoming });
+            }}
+          />
+        </label>
+        <br />
+        <label>
+          Tufts number:{' '}
+          <input
+            type="text"
+            name="tufts_num"
+            maxLength={7}
+            value={student.tufts_num}
+            onChange={(e) => setStudent({ ...student, tufts_num: e.target.value })}
+          />
+        </label>
+        <br />
+        <label>
+          BHCC number:{' '}
+          <input
+            type="text"
+            name="bhcc_num"
+            value={student.bhcc_num}
+            maxLength={32}
+            onChange={(e) => setStudent({ ...student, bhcc_num: e.target.value })}
+          />
+        </label>
+        <br />
+        <label>
+          Parole status:{' '}
+          <textarea
+            name="parole_status"
+            value={student.parole_status}
+            onChange={(e) => setStudent({ ...student, parole_status: e.target.value })}
+          />
+        </label>
+        <br />
+        <label>
+          Student status:{' '}
+          <textarea
+            name="student_status"
+            value={student.student_status}
+            onChange={(e) => setStudent({ ...student, student_status: e.target.value })}
+          ></textarea>
+        </label>
+        <br />
+        <label>
+          Cohort:{' '}
+          <input
+            type="number"
+            name="cohort"
+            value={student.cohort}
+            onChange={(e) => {
+              const incoming: number = e.target.value == '' ? 0 : parseInt(e.target.value);
+              setStudent({ ...student, cohort: incoming });
+            }}
+          />
+        </label>
+        <br />
+        <label>
+          Year given:{' '}
+          <input
+            type="number"
+            name="years_given"
+            value={student.years_given}
+            onChange={(e) => {
+              const incoming: number = e.target.value == '' ? 0 : parseInt(e.target.value);
+              setStudent({ ...student, years_given: incoming });
+            }}
+          />
+        </label>
+        <br />
+        <label>
+          Years left:{' '}
+          <input
+            type="number"
+            name="years_left"
+            value={student.years_left}
+            onChange={(e) => {
+              const incoming: number = e.target.value == '' ? 0 : parseInt(e.target.value);
+              setStudent({ ...student, years_left: incoming });
+            }}
+          />
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </>
   );
 };
 
