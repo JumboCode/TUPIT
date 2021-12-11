5d89354b8b149a341586df004bc46abb287eb794
Tyler <tyler.thompson@tufts.edu>
2021-12-11 13:04:37 -0500

    Change password view and form (#13)
    * Django view to change user password
    * Change password form for logged in users
    * More reliable redirecting depending on user logged status.

--- backend/api/views.py
+++ backend/api/views.py
@@ -89,6 +89,25 @@ def ValidateLoggedIn(request):
     else:
         return JsonResponse({'is_logged_in': False}, status=200)
 
+def ChangePassword(request):
+    if not request.user.is_authenticated:
+        return JsonResponse({'info': 'Not authenticated'}, status=401)
+
+    data = json.loads(request.body)
+    username = request.user.username
+    current_password = data.get('old_password')
+    new_password = data.get('new_password')
+    user = authenticate(request, username=username, password=current_password)
+
+    if user is None:
+        return JsonResponse({'info': 'Invalid credentials'}, status=401)
+
+    user.set_password(new_password)
+    user.save()
+    login(request, user)
+    
+    return JsonResponse({'info': 'Successfully changed password'}, status=200)
+
 # Create a progress report comparing a student's course progress to the courses required by the active degree
 @csrf_exempt
 def AuditStudentProgress(request):
