87bc0f4aca29203ee9274531c287a3230c18e530
Tyler <tyler.thompson@tufts.edu>
2021-12-11 13:04:37 -0500

    Change password view and form (#13)
    * Django view to change user password
    * Change password form for logged in users
    * More reliable redirecting depending on user logged status.

---
+++ frontend/src/pages/changepass.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useAuth } from '../components/auth';
+
+export default function ChangePass() {
+  const { isLoggedIn, csrfToken, login, logout } = useAuth();
+  const router = useRouter();
+
+  function doChangePass(e) {
+    e.preventDefault();
+
+    var old_p = e.target.old_password.value;
+    var new_p = e.target.new_password.value;
+    var new_p_confirm = e.target.new_password_confirm.value;
+
+    // TODO: More robust form validation. This was just quick and easy.
+    if (new_p != new_p_confirm) alert('Passwords must match');
+    else if (new_p == '') alert('Password cannot be blank');
+    else
+      fetch('http://127.0.0.1:8000/change-password/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrfToken,
+        },
+        credentials: 'include',
+        body: JSON.stringify({
+          old_password: old_p,
+          new_password: new_p,
+        }),
+      })
+        .then((res) => {
+          if (res.ok) {
+            alert('Successfully changed password');
+            router.push('/');
+          } else {
+            alert('Password was incorrect');
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          alert('Error connecting to server');
+        });
+  }
+
+  return (
+    <div>
+      <form onSubmit={doChangePass}>
+        <input type="password" name="old_password" placeholder="Current Password" />
+        <input type="password" name="new_password" placeholder="New Password" />
+        <input type="password" name="new_password_confirm" placeholder="Confirm new Password" />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
