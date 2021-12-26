86f6722532b1d3e1c343c76540c92f62291f37a4
Tyler Thompson <ty.9100@gmail.com>
2021-12-26 11:20:39 -0800

    Move changepass page to folder

--- frontend/src/pages/ChangePass/index.tsx
+++ frontend/src/pages/ChangePass/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { useAuth } from '../components/auth';
-import styles from './ChangePass.module.scss';
+import { useAuth } from '../../components/auth';
+import styles from './index.module.scss';
 
 export default function ChangePass() {
   const { isLoggedIn, csrfToken, login, logout } = useAuth();
