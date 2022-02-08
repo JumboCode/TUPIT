bc71841adc89052bec64153f742de7ff57f4ad0b
Michael Simpson <michaelsimpson2019@gmail.com>
2022-02-07 20:35:02 -0500

    Add navbar (#19)
    * initial conversion
    
    * convert index demo page to ts
    
    * initial navbar changes
    
    Co-authored-by: sarahsvahn <sarahsvahn@users.noreply.github.com>
    Co-authored-by: sgilfeather <sgilfeather@users.noreply.github.com>
    
    * Navbar links per page
    
    Co-authored-by: sarahsvahn <sarahsvahn@users.noreply.github.com>
    
    * initial conversion
    
    * convert index demo page to ts
    
    * initial navbar changes
    
    Co-authored-by: sarahsvahn <sarahsvahn@users.noreply.github.com>
    Co-authored-by: sgilfeather <sgilfeather@users.noreply.github.com>
    
    * Navbar links per page
    
    Co-authored-by: sarahsvahn <sarahsvahn@users.noreply.github.com>
    
    * move navbar closer to final
    
    * add logout button
    
    * polish
    
    * add login and logout functionality
    
    Co-authored-by: sarahsvahn <sarahsvahn@users.noreply.github.com>
    Co-authored-by: sgilfeather <sgilfeather@users.noreply.github.com>

---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import Link from 'next/link';
+
+// Import styles
+import styles from './navbar.module.scss';
+import classNames from 'classnames/bind';
+import { useAuth } from './auth';
+import router from 'next/router';
+const cx = classNames.bind(styles);
+
+type NavbarProps = {
+  children?: React.ReactNode;
+  links?: HamburgerLink[];
+  hidden?: boolean;
+};
+
+export type HamburgerLink = {
+  display: string;
+  location: string;
+};
+
+const defaultLinks: HamburgerLink[] = [];
+
+const Navbar: React.FC<NavbarProps> = ({ children, links, hidden }) => {
+  const [menuVisible, setMenuVisible] = React.useState(false);
+  const auth = useAuth();
+  return (
+    <>
+      {hidden == undefined || !hidden ? (
+        <>
+          <div className={cx('navbar-container')}>
+            <Link href="/">
+              <div className={cx('navbar-title')}>TUPIT</div>
+            </Link>
+            <Link href="/Classes">
+              <div className={cx('navbar-link')}>Classes</div>
+            </Link>
+            <Link href="/Students">
+              <div className={cx('navbar-link')}>Students</div>
+            </Link>
+
+            {auth.isLoggedIn ? (
+              <span className={cx('navbar-hamburger')} onClick={() => setMenuVisible(!menuVisible)}>
+                <i className={cx('fa fa-bars')}></i>
+              </span>
+            ) : (
+              <button className={cx('login-button')} onClick={() => router.push('/login')}>
+                Log In
+              </button>
+            )}
+          </div>
+          <div className={cx(menuVisible ? 'navbar-menu' : 'navbar-menu-invisible')}>
+            {(links == undefined ? defaultLinks : links).map((link, index) => (
+              <Link href={link.location} key={index}>
+                <span className={cx('navbar-menu-item')}>{link.display}</span>
+              </Link>
+            ))}
+            <button className={cx('logout-button')} onClick={() => router.push('/logout')}>
+              Log Out
+            </button>
+          </div>
+          {children}
+        </>
+      ) : (
+        <>{children}</>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
