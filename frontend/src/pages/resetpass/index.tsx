3d21238de7005f54c1587b5065d61fb64d79214a
sgilfeather <skylar.gilfeather@gmail.com>
2022-02-27 21:12:44 -0500

    Reset password spacing and scaling

--- frontend/src/pages/resetpass/index.tsx
+++ frontend/src/pages/resetpass/index.tsx
@@ -7,17 +7,33 @@ const ExportPassword = () => {
       <div className={styles.box}>
         <h3>Reset Password</h3>
         <form>
-            <label htmlFor="email">
-                <input id="email" className={styles.reset} type="text" 
-                name="email" placeholder="Enter your email" />
-            </label>
-            <label htmlFor="newpassword">
-                <input id="newpassword" className={styles.reset} type="text" 
-                name="newpassword" placeholder="Enter your new password" />
-            </label>
-        </form> 
-        <div className={styles.buttonReverse}>
-            <button type="button">Next</button>
+          <div className={styles.wrapper}>
+            <div className={styles.row}>
+              <label htmlFor="email">
+                <input
+                  id="email"
+                  className={styles.reset}
+                  type="text"
+                  name="email"
+                  placeholder="Enter your email"
+                />
+              </label>
+            </div>
+            <div className={styles.row}>
+              <label htmlFor="newpassword">
+                <input
+                  id="newpassword"
+                  className={styles.reset}
+                  type="text"
+                  name="newpassword"
+                  placeholder="Enter your new password"
+                />
+              </label>
+            </div>
+          </div>
+        </form>
+        <div className={styles.rowReverse}>
+          <button type="button">Next</button>
         </div>
       </div>
     </div>
