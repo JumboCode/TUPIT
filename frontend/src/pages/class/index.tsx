325d5bb0116bd741ecb99d6412eb22a5c660dcf8
Eddie Hatfield <edward.hatfield@tufts.edu>
2022-03-12 11:02:41 -0500

    Merge please (╯°□°）╯︵ ┻━┻

--- frontend/src/pages/class/index.tsx
+++ frontend/src/pages/class/index.tsx
@@ -1,179 +1,131 @@
-/**
- * @author Sara
- * @author Nick
- *
- * @todo Format and refactor CSS.
- * @todo Refactor code.
- */
+import React, { useEffect, useState, createRef } from 'react';
+import { useRouter } from 'next/router';
+import { useAuth } from '../../components/auth';
+import styles from './index.module.scss';
 
-import React, { useState, useEffect }  from 'react';
+export default function SearchClass() {
+  const { isLoggedIn, csrfToken, login, logout } = useAuth();
+  const router = useRouter();
+  const [results, setResults] = useState([]);
+  const [depOpts, setDepOpts] = useState([]);
 
-import { SearchOptionInterface, SearchBoxInterface } from './interface';
-import styles from './class.module.scss';
-import className from 'classnames/bind';
-
-const cx = className.bind(styles);
-
-async function getCourse(url: string) {
-  const res = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include',
-  });
-  const data = await res.json();
-  if (!data) {
-    throw new Error('Unable to fetch data from Django');
-  }
-  return data;
-}
-
-/**
- *
- * @callback readOption
- * @returns {JSX.element}
- */
-const SearchBox = (props: SearchBoxInterface): JSX.Element =>  {
-  return (
-    <div className={styles.row}>
-      <div className={styles.col_1}>
-        <p>{props.boxName}</p>
-      </div>
-      <div className={`${styles.col} ${styles.selectBox}`}>
-        <select id={props.boxKey} onChange={props.handleQuery}>
-          <option value={''}></option>
-          {Array
-            .from(props.boxValue)
-            .map((entry) => (<option key={entry} value={entry}>{entry}</option>))
-          }
-        </select>
-      </div>
-    </div>
-  )
-};
-
-const Class = () => {
-  const [option, setOption] = useState<SearchOptionInterface>({
-    course_title: new Set(),
-    department: new Set() 
-  });
-  const [query, setQuery] = useState<SearchOptionInterface>({
-    course_title: '',
-    department: '',
-  });
-  const [course, setCourse] = useState<JSX.Element[] | null>(null);
+  const courseTitle = createRef<HTMLInputElement>();
+  const tuftsCourseNum = createRef<HTMLInputElement>();
+  const bunkerCourseNum = createRef<HTMLInputElement>();
+  const department = createRef<HTMLSelectElement>();
 
   useEffect(() => {
-    (async () => {
-      const data = await getCourse('http://127.0.0.1:8000/api/course');
-
-      data.data.forEach((entry) => {
-        for (const [key, value] of Object.entries(entry.attributes)) {
-          if (option.hasOwnProperty(key)) {
-            option[key].add(value);
-          }
-        }
+    async function getClasses() {
+      const res = await fetch('http://127.0.0.1:8000/api/course/', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      }).catch((err) => {
+        alert('Error connecting to server');
+        console.log(err);
       });
-      setOption({ ...option });
-    })();
-  }, []);
-
-  const parseOption = (): void => {
-    let url = 'http://127.0.0.1:8000/api/course?';
-
-    /**
-     * @todo Deal with when user click search but have not make a selection
-     */
-    for (let [key, value] of Object.entries(query)) {
-      if (value) {
-        url += `${key}=${value}&`;
+      if (res && res.ok) {
+        const data = await res.json();
+        setResults(data.data);
       }
     }
-    url = url.slice(0, url.length - 1);
-    getCourse(url)
-      .catch((e) => {
-        setCourse(null);
+
+    async function getDepartmentData() {
+      fetch('http://127.0.0.1:8000/api/course/', {
+        method: 'OPTIONS',
+        headers: {
+          'Content-Type': 'application/vnd.api+json',
+          'X-CSRFToken': csrfToken,
+        },
+        credentials: 'include',
       })
-      .then((data) => {
-        if (data.hasOwnProperty('data')) {
-          const tmp = data.data.map((attributes) => (
-            <ul key={attributes.id}>
-              <li>{attributes.attributes.course_title}</li>
-            </ul>
-          ));
-          setCourse(tmp);
-        }
-      });
-  };
+        .then((res) => res.json())
+        .then((res) => {
+          let deps = [];
+          res.data.actions.POST.department.choices.map((dep) =>
+            deps.push({ name: dep.display_name, value: dep.value })
+          );
+          setDepOpts(deps);
+        });
+    }
 
-  /**
-   * Set state of option when a selection is made.
-   *
-   * @param {React.ChangeEventHandler<HTMLSelectElement>} - onChange event handler
-   * @todo Figure out what's the type of e
-   */
-  const handleQuery = (e: React.ChangeEvent<HTMLSelectElement>): void => {
-    query[e.target.id] = e.target.value;
-    setQuery(query);
-  };
+    getClasses();
+    getDepartmentData();
+  }, []);
 
-  return (
-    <main className={styles.container}>
-      <div className={styles.box}>
-        <h1>Search Classes</h1>
-        <SearchBox 
-          boxName={'Course'}
-          boxKey={'course_title'}
-          boxValue={option.course_title as Set<string>}
-          handleQuery={handleQuery}
-        />
-        <SearchBox
-          boxName={'Department'}
-          boxKey={'department'}
-          boxValue={option.department as Set<string>}
-          handleQuery={handleQuery}
-        />
-        <div className={styles.button}>
-          <button onClick={parseOption}>Search</button>
+  async function onSearch(e) {
+    e.preventDefault();
+
+    const query = `?course_title__icontains=${courseTitle.current.value}&course_number_tufts__icontains=${tuftsCourseNum.current.value}&course_number_bhcc__icontains=${bunkerCourseNum.current.value}&department=${department.current.value}`;
+
+    const res = await fetch(`http://127.0.0.1:8000/api/course/${query}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    }).catch((err) => {
+      alert('Error connecting to server');
+      console.log(err);
+    });
+    if (res && res.ok) {
+      const data = await res.json();
+      setResults(data.data);
+    }
+  }
+
+  function classResult(course) {
+    return (
+      <div className={styles.result} onClick={() => router.push(`course/${course.id}`)}>
+        <div className={styles.courseTitle}>{`${course.attributes.course_title}`}</div>
+        <div className={styles.courseInfo}>
+          <span>Tufts: {`${course.attributes.course_number_tufts}`}</span>
+          <span>BHCC: {`${course.attributes.course_number_bhcc}`}</span>
+          <span>Department: {`${course.attributes.department}`}</span>
         </div>
       </div>
-      {course}
-    </main>
+    );
+  }
+
+  return (
+    <div className={styles.windowContainer}>
+      <div className={styles.window}>
+        <form onSubmit={onSearch} className={styles.filterContainer}>
+          <div className={styles.filterFields}>
+            <div className={styles.label}>Course Title</div>
+            <input type="text" ref={courseTitle} />
+            <div className={styles.label}>Tufts Course Number</div>
+            <input type="text" ref={tuftsCourseNum} />
+            <div className={styles.label}>Bunker Course Number</div>
+            <input type="text" ref={bunkerCourseNum} />
+            <div className={styles.label}>Department</div>
+            <select name="department" className={styles.select} ref={department}>
+              <option></option>
+              {depOpts.map((dep) => (
+                <option key={dep.value} value={dep.value}>
+                  {dep.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <input className={styles.button} type="submit" value="Search" />
+        </form>
+        <div className={styles.resultsContainer}>
+          <div className={styles.results}>
+            {results &&
+              (results.length > 0 ? (
+                results.map(classResult)
+              ) : (
+                <div className={styles.noResults}>No results found</div>
+              ))}
+          </div>
+          <div className={styles.button} onClick={() => router.push('/class/add')}>
+            +
+          </div>
+        </div>
+      </div>
+    </div>
   );
-};
-
-export default Class;
-
-// // class Requirement extends Component {
-// //   constructor(props) {
-// //     super(props);
-// //     this.state = {
-// //       requirement: {
-// //         COMP15: ['COMP11', 'COMP160'],
-// //         LATIN3: ['LATIN1', 'LATIN2'],
-// //       },
-// //     };
-// //   }
-
-// //   /**
-// //    * Map course requirement as a list.
-// //    */
-// //   renderRequirement() {
-// //     const lookup = Object.hasOwnProperty;
-// //     if (!lookup.call(this.state.requirement, this.props.course)) {
-// //       throw 'Course is not defined. Please check Search component dropdown.';
-// //     }
-
-// //     const requirement = this.state.requirement[this.props.course];
-// //     return requirement.map((course) => <li key={course}>{course}</li>);
-// //   }
-
-// //   render() {
-// //     return (
-// //       <div>
-// //         <ul>{this.renderRequirement()}</ul>
-// //       </div>
-// //     );
-// //   }
-// // }
+}
