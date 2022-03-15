c13424797bfa4dfe305e71a0ac509241385b0c31
Nick Doan <nickbar01234@gmail.com>
2022-03-15 14:14:51 -0400

    Reset Password + Refactoring (#30)
    * Initial reset password page. Work with Nick Doan
    
    Fix
    
    * added scss colors to use and base fonts/size
    
    * Add label
    
    * Reset password spacing and scaling
    
    * Final resetpass styling
    
    * Initial reset password page. Work with Nick Doan
    
    Fix
    
    * Add label
    
    * Reset password spacing and scaling
    
    * Final resetpass styling
    
    * Pass request to backend
    
    * Initial login modularization
    
    * Refactor authentication box
    
    * Fix password page
    
    Co-authored-by: sgilfeather <skylar.gilfeather@gmail.com>
    Co-authored-by: Jackson Parsells <john.parsells@mumvia.com>

---
+++ frontend/src/pages/resetpass/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import AuthBox from '../../components/authbox';
+
+const ExportResetPassword = () => {
+  const callback = (e) => {
+    e.preventDefault();
+    console.log("HI");
+  };
+
+  const content = [
+    (<label htmlFor='email'>
+      <input id='email' type='text' name='email' placeholder='Enter your email'/>
+     </label>),
+    (<label htmlFor='password'>
+      <input id='password' type='text' name='password' placeholder='Enter your password'/>
+    </label>) 
+  ];
+
+  return (
+    <AuthBox header={'Reset Password'}
+     content={content}
+     callback={callback}
+     navigate={'Next'} />
+  );
+};
+
+export default ExportResetPassword;
