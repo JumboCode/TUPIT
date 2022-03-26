6c175ddc3735cbbb01e35a7e6bc0c56136d08597
nickbar01234 <nickbar01234@gmail.com>
2022-03-25 23:57:52 -0400

    Add form validation for login

--- frontend/src/components/authbox.tsx
+++ frontend/src/components/authbox.tsx
@@ -2,32 +2,19 @@ import React from 'react';
 import styles from './authbox.module.scss';
 
 interface AuthBoxInterface {
-  header: string;
-  callback: (e) => void;
-  content: JSX.Element[];
-  navigate: string;
+  children: JSX.Element[];
 };
 
 /*
- * Used for login and password changing page.
+ * Generic centered-box grid for input forms.
  */
 const AuthBox = (props: AuthBoxInterface) => {
   return (
     <div className={styles.container}>
       <div className={styles.box}>
-        <h3>{props.header}</h3>
-        <form onSubmit={props.callback}>
-          <div className={styles.wrapper}>
-            {props.content.map((x: JSX.Element) => (
-              <div className={styles.row}>
-                {x}
-              </div>
-            ))}
-          </div>
-          <div className={styles.rowReverse}>
-            <button type='submit'>{props.navigate}</button>
-          </div>
-        </form>
+        <div className={styles.wrapper}>
+          {props.children}
+        </div>
       </div>
     </div>
   );
