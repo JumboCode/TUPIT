3cab1ab78cc8461cd92d07cd973175deb09a5957
nickbar01234 <nickbar01234@gmail.com>
2022-03-30 14:18:17 -0400

    Update reset password

--- frontend/src/pages/resetpass/index.tsx
+++ frontend/src/pages/resetpass/index.tsx
@@ -1,27 +1,50 @@
 import React from 'react';
-import AuthBox from "@/components/authbox";
+import { useForm } from 'react-hook-form';
+import AuthBox from '@/components/authbox';
 
-const ExportResetPassword = () => {
-  const callback = (e) => {
-    e.preventDefault();
-    console.log("HI");
-  };
-
-  const content = [
-    (<label htmlFor='email'>
-      <input id='email' type='text' name='email' placeholder='Enter your email'/>
-     </label>),
-    (<label htmlFor='password'>
-      <input id='password' type='text' name='password' placeholder='Enter your password'/>
-    </label>) 
-  ];
+const ResetPassword = () => {
+  const { register, handleSubmit, formState: { errors }} = useForm();
 
   return (
-    <AuthBox header={'Reset Password'}
-     content={content}
-     callback={callback}
-     navigate={'Next'} />
+    <AuthBox>
+      <h3>Reset Password</h3>
+      <form>
+        <input type='text' placeholder='Enter your email'
+         {...register('email', {
+           required: {
+             value: true,
+             message: 'Email must not be empty'
+           }
+         })}/>
+         {errors.email && <p>{errors.email.message}</p>}
+         <button type='submit'>Next</button>
+      </form>
+    </AuthBox>
   );
 };
 
-export default ExportResetPassword;
+export default ResetPassword;
+// const ExportResetPassword = () => {
+//   const callback = (e) => {
+//     e.preventDefault();
+//     console.log("HI");
+//   };
+
+//   const content = [
+//     (<label htmlFor='email'>
+//       <input id='email' type='text' name='email' placeholder='Enter your email'/>
+//      </label>),
+//     (<label htmlFor='password'>
+//       <input id='password' type='text' name='password' placeholder='Enter your password'/>
+//     </label>) 
+//   ];
+
+//   return (
+//     <AuthBox header={'Reset Password'}
+//      content={content}
+//      callback={callback}
+//      navigate={'Next'} />
+//   );
+// };
+
+// export default ExportResetPassword;
