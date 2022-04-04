e39fc368d0d0ac4f4a0e88f045827ba30aef5baf
nickbar01234 <nickbar01234@gmail.com>
2022-04-04 11:23:51 -0400

    Move Authbox CSS to login css

--- frontend/src/components/authbox.tsx
+++ frontend/src/components/authbox.tsx
@@ -12,9 +12,9 @@ const AuthBox = (props: AuthBoxInterface) => {
   return (
     <div className={styles.container}>
       <div className={styles.box}>
-        <div className={styles.wrapper}>
-          {props.children}
-        </div>
+        {/* <div className={styles.wrapper}> */}
+        {props.children}
+        {/* </div> */}
       </div>
     </div>
   );
