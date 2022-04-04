02e05af9fd7174ff40acfe177ff28eab4320a1e4
Michael Simpson <michaelsimpson2019@gmail.com>
2022-04-04 15:15:33 -0400

    Add dashboard/homepage search (#37)
    * dashboard search
    
    Co-authored-by: taarikabala1 <taarikabala1@users.noreply.github.com>
    
    * check for undefined
    
    * Undefined catch
    
    * update styling!
    
    Co-authored-by: taarikabala1 <taarikabala1@users.noreply.github.com>
    
    * Fixed bugs when searching for classes with an initial course title
    
    Co-authored-by: taarikabala1 <taarikabala1@users.noreply.github.com>
    Co-authored-by: Tyler Thompson <tyler.thompson@tufts.edu>

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { createRef, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useAuth } from '../components/auth';
 
@@ -11,20 +11,54 @@ const Home: React.FC = () => {
   const { isLoggedIn, csrfToken, login, logout } = useAuth();
   const router = useRouter();
 
+  const cohort = createRef<HTMLInputElement>();
+  const course = createRef<HTMLInputElement>();
+
   return (
     <div className={cx('base')}>
-      <h1>Hello, TUPIT!</h1>
-      {isLoggedIn ? (
-        <input type="button" onClick={() => router.push('/logout')} value="Logout" />
-      ) : (
-        <input type="button" onClick={() => router.push('/login')} value="Login" />
-      )}
-      <br />
-      {isLoggedIn ? (
-        <input type="button" onClick={() => router.push('/ChangePass')} value="Change Password" />
-      ) : (
-        ''
-      )}
+      <div className={cx('dashboard')}>
+        <h1>TUPIT Dashboard</h1>
+        {isLoggedIn ? null : (
+          <input type="button" onClick={() => router.push('/login')} value="Login" />
+        )}
+        <br />
+        {isLoggedIn ? (
+          <div className={cx('dashboard_content')}>
+            <div>
+              <h2>Cohort Search</h2>
+              <form
+                className={cx('search_field')}
+                onSubmit={() => router.push(`/student/?cohortInit=${cohort.current.value}`)}
+              >
+                <input
+                  className={cx('text_field')}
+                  type="number"
+                  ref={cohort}
+                  min={0}
+                  onWheel={(e) => e.currentTarget.blur()}
+                />
+                <input className={cx('button')} type="submit" value="Search" />
+              </form>
+            </div>
+            <div>
+              <h2>Class Search</h2>
+              <form
+                className={cx('search_field')}
+                onSubmit={() => router.push(`/class/?courseTitleInit=${course.current.value}`)}
+              >
+                <input className={cx('text_field')} type="string" ref={course} />
+                <input className={cx('button')} type="submit" value="Search" />
+              </form>
+            </div>
+            <input
+              className={cx('button')}
+              type="button"
+              onClick={() => router.push('/ChangePass')}
+              value="Change Password"
+            />
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
