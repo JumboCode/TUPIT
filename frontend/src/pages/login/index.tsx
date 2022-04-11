b04596e9f226aa12959bcf1e0d60c9563c9a1fe6
sarahsvahn <92219740+sarahsvahn@users.noreply.github.com>
2022-04-11 19:12:34 -0400

    updated style of login page to match new designs (#26)
    * updated style of login page to match new designs
    
    Co-authored-by: Sarah Svahn <sarahsvahn@MacBook-Pro-152.local>
    Co-authored-by: Tyler Thompson <tyler.thompson@tufts.edu>

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { useAuth } from "@/components/auth";
-import AuthBox from "@/components/authbox";
+import { useAuth } from '@/components/auth';
+import AuthBox from '@/components/authbox';
 import styles from './index.module.scss';
 
 export default function LoginForm() {
   const { isLoggedIn, csrfToken, login, logout } = useAuth();
   const router = useRouter();
+
+  const [showPassword, setShowPassword] = useState(false);
 
   useEffect(() => {
     if (isLoggedIn) router.push('/');
@@ -20,33 +22,63 @@ export default function LoginForm() {
     );
   }
 
+  // return (
+  //   <div className={styles.container}>
+  //     <form className={styles.loginForm} onSubmit={doLogin}>
+  //       <div className={styles.signInTitle}>Sign in</div>
+  //       <input
+  //         className={styles.textField}
+  //         type="text"
+  //         name="username"
+  //         placeholder="Enter your username"
+  //       />
+  //       <input
+  //         className={styles.textField}
+  //         type={showPassword ? 'text' : 'password'}
+  //         name="password"
+  //         placeholder="Enter your password"
+  //       />
+  //       <label className={styles.showPassword}>
+  //         <input
+  //           type="checkbox"
+  //           checked={showPassword}
+  //           onClick={() => setShowPassword(!showPassword)}
+  //         />
+  //         <>Show Password</>
+  //       </label>
+  //       <div className={styles.submitContainer}>
+  //         <p className={styles.forgotPasswordText}>Forgot password?</p>
+  //         <input className={styles.btn} type="submit" value="Next" />
+  //       </div>
+  //     </form>
+  //   </div>
+  // );
+
   function resetPass(e) {
     router.push('/resetpass');
   }
-  
+
   const content = [
-    (<label htmlFor='username'>
-      <input id='username'
-       type='text'
-       name='username' 
-       placeholder='Enter your username'
+    <label htmlFor="username">
+      <input id="username" type="text" name="username" placeholder="Enter your username" />
+    </label>,
+    <label htmlFor="password">
+      <input
+        id="password"
+        type={showPassword ? 'text' : 'password'}
+        name="password"
+        placeholder="Enter your password"
       />
-    </label>),
-    (<label htmlFor='password'>
-      <input id='password' 
-       type='password'
-       name='password'
-       placeholder='Enter your password'
+    </label>,
+    <label className={styles.showPassword}>
+      <input
+        type="checkbox"
+        checked={showPassword}
+        onClick={() => setShowPassword(!showPassword)}
       />
-    </label>),
-    (<a onClick={resetPass}>
-      Forgot your password?{' '}
-    </a>)
+      <div>Show Password</div>
+    </label>,
+    <a onClick={resetPass}>Forgot your password? </a>,
   ];
-  return (
-    <AuthBox header={'Login'}
-     callback={doLogin}
-     content={content}
-     navigate={'Next'}/>
-  ); 
+  return <AuthBox header={'Login'} callback={doLogin} content={content} navigate={'Next'} />;
 }
