f9fe55f9d2199a2670f8e6e433fddc6dde5380f2
Tyler Thompson <tyler.thompson@tufts.edu>
2022-04-18 20:51:35 -0400

    Finish degree req page skeleton

--- frontend/src/pages/degree/req/index.tsx
+++ frontend/src/pages/degree/req/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import { useRouter } from 'next/router';
 import { useAuth } from '@/components/auth';
 import CourseSelector from '../../../components/Selectors/CourseSelector';
@@ -6,9 +6,10 @@ import CourseSelector from '../../../components/Selectors/CourseSelector';
 export default function EditDegreeReqs() {
   const { isLoggedIn, csrfToken, login, logout } = useAuth();
   const [master, setMaster] = useState([]);
+  const [results, setResults] = useState([]);
   const [selectedReq, setSelectedReq] = useState(null);
   const [showCourseSelector, setShowCourseSelector] = useState(false);
-  const router = useRouter();
+  const [selectedReqTitle, setSelectedReqTitle] = useState('');
 
   async function fetchDegreeReqData() {
     const url = 'http://127.0.0.1:8000/api/degreerequirement/';
@@ -26,6 +27,7 @@ export default function EditDegreeReqs() {
     if (res) {
       const data = await res.json();
       setMaster(data.data);
+      setResults(data.data);
     }
   }
 
@@ -33,8 +35,18 @@ export default function EditDegreeReqs() {
     fetchDegreeReqData();
   }, []);
 
-  function saveSelectedReq() {
-    const url = `http://127.0.0.1:8000/api/degreerequirement/' ${selectedReq.req.id}/`;
+  function onSearch(e) {
+    const search = e.target.value;
+    const results = master.filter((req) => {
+      return req.attributes.title.toLowerCase().includes(search.toLowerCase());
+    });
+    setResults(results);
+  }
+
+  function saveSelectedReq(e) {
+    e.preventDefault();
+
+    const url = `http://127.0.0.1:8000/api/degreerequirement/${selectedReq.req.id}/`;
     fetch(url, {
       method: 'PUT',
       headers: {
@@ -47,7 +59,7 @@ export default function EditDegreeReqs() {
           type: 'DegreeRequirement',
           id: selectedReq.req.id,
           attributes: {
-            title: selectedReq.req.attributes.title,
+            title: e.target.title.value,
             fulfilled_by: selectedReq.courses.map(
               (course) => `http://127.0.0.1:8000/api/course/${course.id}/`
             ),
@@ -69,6 +81,60 @@ export default function EditDegreeReqs() {
       });
   }
 
+  function deleteSelectedReq() {
+    const url = `http://127.0.0.1:8000/api/degreerequirement/${selectedReq.req.id}/`;
+    fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+        'X-CSRFToken': csrfToken,
+      },
+      credentials: 'include',
+    })
+      .then((res) => {
+        if (res.ok) {
+          alert('Successfully deleted degree requirement');
+        } else {
+          alert('Error deleting degree requirement');
+        }
+        fetchDegreeReqData();
+        setSelectedReq(null);
+      })
+      .catch((err) => {
+        alert('Error connecting to server');
+        console.log(err);
+      });
+  }
+
+  function newDegreeReq() {
+    const url = 'http://127.0.0.1:8000/api/degreerequirement/';
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+        'X-CSRFToken': csrfToken,
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        data: {
+          type: 'DegreeRequirement',
+          attributes: {
+            title: 'New Degree Requirement',
+            fulfilled_by: [],
+          },
+        },
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) alert('Error creating degree requirement');
+        fetchDegreeReqData();
+      })
+      .catch((err) => {
+        alert('Error connecting to server');
+        console.log(err);
+      });
+  }
+
   function addReqCourse(course) {
     let reqData = {
       req: selectedReq.req,
@@ -77,7 +143,16 @@ export default function EditDegreeReqs() {
     setSelectedReq(reqData);
   }
 
+  function removeReqCourse(course) {
+    let reqData = {
+      req: selectedReq.req,
+      courses: selectedReq.courses.filter((c) => c.id !== course.id),
+    };
+    setSelectedReq(reqData);
+  }
+
   async function setSelectedReqData(req) {
+    setSelectedReq(null);
     let reqData = {
       req: null,
       courses: [],
@@ -94,21 +169,8 @@ export default function EditDegreeReqs() {
     setSelectedReq(reqData);
   }
 
-  function reqEntry(req) {
-    return (
-      <div
-        onClick={() => {
-          setSelectedReqData(req);
-        }}
-        key={req.id}
-      >
-        <div>{req.attributes.title}</div>
-      </div>
-    );
-  }
-
   async function getCourseTitle(id) {
-    let url = 'http://127.0.0.1:8000/api/course/' + id + '/';
+    let url = `http://127.0.0.1:8000/api/course/${id}/`;
     const res = await fetch(url, {
       method: 'GET',
       headers: {
@@ -123,27 +185,46 @@ export default function EditDegreeReqs() {
     return null;
   }
 
+  function reqEntry(req) {
+    return (
+      <div
+        onClick={() => {
+          setSelectedReqData(req);
+        }}
+        key={req.id}
+      >
+        <div>{req.attributes.title}</div>
+      </div>
+    );
+  }
+
   function fulfilledByEntry(course) {
     return (
       <div key={course.id}>
         <div>{course.course_title}</div>
+        <div onClick={() => removeReqCourse(course)}>X</div>
       </div>
     );
   }
 
   return (
     <div>
-      <div>{master.length > 0 ? master.map(reqEntry) : <div>No results found</div>}</div>
+      <input type="text" placeholder="Search by Title" onChange={onSearch} />
+      <div>{results.length > 0 ? results.map(reqEntry) : <div>No results found</div>}</div>
+      <div onClick={newDegreeReq}>New</div>
       <br />
       <br />
       <div>
         {selectedReq ? (
-          <div>
-            {selectedReq.req.attributes.title}
+          <form onSubmit={saveSelectedReq}>
+            <div key={selectedReq.req.attributes.title}>
+              <input type="text" name="title" defaultValue={selectedReq.req.attributes.title} />
+            </div>
             {selectedReq.courses.map(fulfilledByEntry)}
             <div onClick={() => setShowCourseSelector(true)}>+</div>
-            <div onClick={saveSelectedReq}>Save</div>
-          </div>
+            <div onClick={deleteSelectedReq}>Delete</div>
+            <input type="submit" value="Save" />
+          </form>
         ) : null}
       </div>
 
